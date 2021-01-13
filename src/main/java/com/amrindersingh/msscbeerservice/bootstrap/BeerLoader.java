9ca47e6fbecbe08de6cf5b7a73d036800f834be0
Amrinder singh <amrinders433@gmail.com>
2021-01-13 07:17:10 -0500

    Refactored to common pom

--- src/main/java/com/amrindersingh/msscbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/com/amrindersingh/msscbeerservice/bootstrap/BeerLoader.java
@@ -1,14 +1,18 @@
 package com.amrindersingh.msscbeerservice.bootstrap;
 
+import com.amrindersingh.msscbeerservice.domain.Beer;
 import com.amrindersingh.msscbeerservice.repositories.BeerRepository;
+import com.amrindersingh.msscbeerservice.web.model.BeerStyleEnum;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 /**
  * Created by jt on 2019-05-17.
  */
-//@Component
+@Component
 public class BeerLoader implements CommandLineRunner {
 
     public static final String BEER_1_UPC = "0631234200036";
@@ -26,7 +30,43 @@ public class BeerLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        //using data.sql file now
-        //   loadBeerObjects();
+
+        if(beerRepository.count() == 0 ) {
+            loadBeerObjects();
+        }
+    }
+
+    private void loadBeerObjects() {
+        Beer b1 = Beer.builder()
+                .id(BEER_1_UUID)
+                .beerName("Mango Bobs")
+                .beerStyle(BeerStyleEnum.IPA.name())
+                .minOnHand(12)
+                .quantityToBrew(200)
+                .price(new BigDecimal("12.95"))
+                .upc(BEER_1_UPC)
+                .build();
+
+        Beer b2 = Beer.builder()
+                .beerName("Galaxy Cat")
+                .beerStyle(BeerStyleEnum.PALE_ALE.name())
+                .minOnHand(12)
+                .quantityToBrew(200)
+                .price(new BigDecimal("12.95"))
+                .upc(BEER_2_UPC)
+                .build();
+
+        Beer b3 = Beer.builder()
+                .beerName("Pinball Porter")
+                .beerStyle(BeerStyleEnum.PALE_ALE.name())
+                .minOnHand(12)
+                .quantityToBrew(200)
+                .price(new BigDecimal("12.95"))
+                .upc(BEER_3_UPC)
+                .build();
+
+        beerRepository.save(b1);
+        beerRepository.save(b2);
+        beerRepository.save(b3);
     }
 }
--- target/generated-sources/annotations/com/amrindersingh/msscbeerservice/web/mappers/BeerMapperImpl_.java
+++ target/generated-sources/annotations/com/amrindersingh/msscbeerservice/web/mappers/BeerMapperImpl_.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-01-12T06:44:37-0500",
+    date = "2021-01-13T07:13:49-0500",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 15.0.1 (Oracle Corporation)"
 )
 @Component
