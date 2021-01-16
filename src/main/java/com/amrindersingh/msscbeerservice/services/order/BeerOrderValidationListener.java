a999a34f8f91c8699cdde1723421eee33e62da2f
Amrinder singh <amrinders433@gmail.com>
2021-01-16 07:26:53 -0500

    COmpleted validation and allocation saga

---
+++ src/main/java/com/amrindersingh/msscbeerservice/services/order/BeerOrderValidationListener.java
@@ -0,0 +1,28 @@
+package com.amrindersingh.msscbeerservice.services.order;
+
+import com.amrindersingh.msscbeerservice.config.JmsConfig;
+import com.brewery.events.ValidateOrderRequest;
+import com.brewery.events.ValidateOrderResult;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class BeerOrderValidationListener {
+
+    private final BeerOrderValidator validator;
+    private final JmsTemplate jmsTemplate;
+
+    @JmsListener(destination = JmsConfig.VALIDATE_ORDER_QUEUE)
+    public void listen(ValidateOrderRequest validateOrderRequest){
+        Boolean isValid = validator.validateOrder(validateOrderRequest.getBeerOrder());
+
+        jmsTemplate.convertAndSend(JmsConfig.VALIDATE_ORDER_RESPONSE_QUEUE,
+                ValidateOrderResult.builder()
+                        .isValid(isValid)
+                        .orderId(validateOrderRequest.getBeerOrder().getId())
+                        .build());
+    }
+}
